57e586156866762c5ef8d704203d2209728caebb
Melania <bokacmelania@gmail.com>
2026-09-07 22:03:43 +0300

    Вкладені цикли(for).Практична.

---
+++ 05_IncludeCycles/05_IncludeCycles.cpp
@@ -0,0 +1,251 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+	///* for (int i = 1; i <= 10; i++)
+	// {
+	//	 for (int j = 1; j <= 10; j++)
+	//	 {
+	//		 cout << i << " * " << j << " = " << i * j << endl;
+	//	 }
+	//	 cout << "\n______________________________\n";
+	// }*/
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	for (int j = 0; j < 10; j++)
+	//	{
+	//		cout << "* ";
+	//	}
+	//	cout << endl;
+	//}
+	//cout << endl;
+	//int count_star;
+	//int lenght = 10;
+
+	//int star_lines = 1;
+	//while (star_lines <= lenght)
+	//{
+	//	count_star = 1;
+	//	while (count_star <= lenght)
+	//	{
+	//		cout << "* ";
+	//		count_star++;
+	//	}
+	//	cout << endl;
+	//	star_lines++;
+	//}
+
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	for (int j = 0; j < 4; j++)
+	//	{
+	//		cout << " +";
+	//	}
+	//	cout << endl;
+	//}
+	////main axis
+	//for (int i = 0; i < 15; i++)
+	//{
+	//	for (int j = 0; j < 15; j++)
+	//	{
+	//		if (i == j)
+	//		{
+	//			cout << "= ";
+	//		}
+	//		else
+	//		{
+	//			cout << "+ ";
+	//		}
+	//	}
+	//	cout << endl;
+	//}
+	//cout << endl;
+
+
+
+	//for (int i = 0; i < 15; i++)
+	//{
+	//	for (int j = 0; j < 15; j++)
+	//	{
+	//		if (i + j == 15 - 1)
+	//		{
+	//			cout << "= ";
+	//		}
+	//		else
+	//		{
+	//			cout << "+ ";
+	//		}
+	//	}
+	//	cout << endl;
+	//}
+
+
+
+
+
+	//int N = 10;
+	//for (int i = 0; i < N; i++)
+	//{
+	//	for (int j = 0; j < N; j++)
+	//	{
+
+	//		if (i >= j && i + j >= N - 1)
+	//		{
+	//			cout << "|===|";
+	//		}
+	//		else
+	//		{
+	//			cout << "     ";
+	//		}
+
+	//	}
+	//	cout << endl;
+	//}
+
+	//for (int i = 0; i < 7; i++)
+	//{
+	//	for (int j = 0; j < 10; j++)
+	//	{
+	//		cout << "|###|";
+	//	}
+	//	cout << endl;
+	//}
+
+
+//a
+//int N = 6;
+//for (int i = 0; i < 6; i++)
+//{
+//	for (int j = 0; j < 6; j++)
+//	{
+//		if (i < j) cout << "* "; else cout << "  ";
+//	} cout << endl;
+//}
+
+//á
+//int N = 6;
+//for (int i = 0; i < 6; i++)
+//{
+//	for (int j = 0; j < 6; j++)
+//	{
+//		if (i > j)
+//			cout << "* ";
+//	
+//		else cout << "  ";
+//	} cout << endl;
+//}
+
+//â
+//int N = 6;
+//for (int i = 0; i < 6; i++)
+//{
+//	for (int j = 0; j < 6; j++)
+//	{
+//		if (j >= i && j <= 6 - 1 - i) cout << "* "; else cout << "  ";
+//	}
+//	cout << endl;
+//}
+
+//ã
+//int N = 6;
+//for (int i = 0; i < 6; i++)
+//{
+//	for (int j = 0; j < 6; j++)
+//	{
+//		if (j < i &&  i +j > 6 - 1) cout << "* "; else cout << "  ";
+//	}
+//	cout << endl;
+//}
+
+//ä
+//int N = 6;
+//for (int i = 0; i < N; i++)
+//{
+//	for (int j = 0; j < N; j++)
+//	{
+//		if ((i < j && i + j < N - 1) || (i > j && i + j > N - 1)) cout << "* "; else cout << "  ";
+//	}
+//	cout << endl;
+//}
+
+
+//e
+//int N = 6;
+//for (int i = 0; i < N; i++)
+//{
+//	for (int j = 0; j < N; j++)
+//	{
+//		if ((i > j && i + j < N - 1) || (i < j && i + j > N - 1)) cout << "* "; else cout << "  ";
+//	}
+//	cout << endl;
+//}
+
+
+//è
+//int N = 6;
+//for (int i = 0; i < N; i++)
+//{
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i+ j< N -1) cout << "* "; else cout << "  ";
+//	}
+//	cout << endl;
+//}
+
+//ê
+//int N = 6;
+//for (int i = 0; i < N; i++)
+//{
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (i+ j> N -1) cout << "* "; else cout << "  ";
+//	}
+//	cout << endl;
+//}
+
+// ç
+//int N = 6;
+//for (int i = 0; i < N; i++)
+//{
+//	for (int j = 0; j < N; j++)
+//	{
+//		if (j > i && i + j > N - 1) cout << "* "; else cout << "  ";
+//	}
+//	cout << endl;
+//}
+
+//æ
+//int N = 6;
+//for (int i = 0; i < 6; i++)
+//{
+//	for (int j = 0; j < 6; j++)
+//	{
+//		if (j <= i && j <= 6 - 1 - i) cout << "* "; else cout << "  ";
+//	}
+//	cout << endl;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
